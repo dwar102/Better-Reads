9caf645268a427e958532c9366b633bb6c16455f
mbaehr <markbaehr@u.boisestate.edu>
2020-12-17 11:50:00 -0600

    Adding Media, MediaType, Shelf and ShelfAssignment beans

---
+++ BetterReads/src/main/java/shrews/MediaType.java
@@ -0,0 +1,96 @@
+package dev.shrews;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="media_types")
+public class MediaType {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	@ManyToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="genre_id")
+	private Genre genre;
+	public MediaType(Integer id, String name, Genre genre) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.genre = genre;
+	}
+	public MediaType() {
+		super();
+		this.id =  0;
+		this.name = "";
+		this.genre = new Genre();
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Genre getGenre() {
+		return genre;
+	}
+	public void setGenre(Genre genre) {
+		this.genre = genre;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((genre == null) ? 0 : genre.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MediaType other = (MediaType) obj;
+		if (genre == null) {
+			if (other.genre != null)
+				return false;
+		} else if (!genre.equals(other.genre))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "MediaType [id=" + id + ", name=" + name + ", genre=" + genre + "]";
+	}
+	
+	
+	
+
+}
