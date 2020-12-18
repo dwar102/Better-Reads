93dd81acdf12fef528b7dc1c8987b2dc537865c0
dwar102 <david_warrington@outlook.com>
2020-12-18 16:58:41 -0500

    Skeleton of User controller and DAOs + HibernateUtil

---
+++ BetterReads/src/main/java/dev/shrews/services/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package dev.shrews.services;
+
+import java.util.Set;
+
+import dev.shrews.beans.User;
+import dev.shrews.exceptions.NonUniqueUsernameException;
+
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public Integer addUser(User u) throws NonUniqueUsernameException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<User> getUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User getUserById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User getUserByName(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
