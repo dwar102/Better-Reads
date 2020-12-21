008001b83a17659074e96cc3a8031382f24b5e47
Brian Conlon <brian.conlon18@myhunter.cuny.edu>
2020-12-21 17:11:14 -0500

    Forgot to push

--- BetterReads/src/main/java/dev/shrews/data/UserDAO.java
+++ BetterReads/src/main/java/dev/shrews/data/UserDAO.java
@@ -5,8 +5,11 @@ import java.util.Set;
 import dev.shrews.beans.User;
 import dev.shrews.exceptions.NonUniqueUsernameException;
 
-public interface UserDAO extends GenericDAO {
+public interface UserDAO extends GenericDAO<User> {
 	public User add(User u) throws NonUniqueUsernameException ;
-	public User getByUsrname(String username);
+	public User getByUsername(String username);
+	public User getUserById(Integer id);
+	public void delete(User u); // No such user exception
+	public Set<User> getUsers();
 }
 
--- BetterReads/src/test/java/dev/shrews/tests/userTest.java
+++ BetterReads/src/test/java/dev/shrews/tests/userTest.java
@@ -1,12 +1,14 @@
 package dev.shrews.tests;
 
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.HashSet;
 import java.util.Set;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -16,11 +18,15 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import dev.shrews.beans.*;
 import dev.shrews.services.*;
 import dev.shrews.data.*;
+import dev.shrews.exceptions.NonUniqueUsernameException;
 
 @ExtendWith(MockitoExtension.class)
 public class userTest {
-	@Mock
 	static UserDAO userDao;
+	
+	@BeforeAll public static void setUp() {
+		userDao = new UserHibernate();
+	}
 	
 	@InjectMocks
 	static UserServiceImpl userServ;
@@ -36,4 +42,15 @@ public class userTest {
 		User u2 = new User();
 		u2.setId(userSequenceMock++);
 	}
-}
+	@Test
+	public void testAddandDelete() throws NonUniqueUsernameException { // testing hibernate connection; not using mockito
+		User u = new User();
+		u.setUsername("Peter");
+		u.setId(userSequenceMock);
+		u = userDao.add(u);
+		System.out.println(u);
+		assertTrue(u.getId() != 0);
+		userDao.delete(u);
+		}
+	}
+
