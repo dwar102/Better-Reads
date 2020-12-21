1f5e37102a50c111b0e0fa2df39900f02c131837
dwar102 <david_warrington@outlook.com>
2020-12-21 14:50:30 -0500

    connect UserController through to UserHibernate

--- BetterReads/src/main/java/dev/shrews/services/UserServiceImpl.java
+++ BetterReads/src/main/java/dev/shrews/services/UserServiceImpl.java
@@ -3,14 +3,21 @@ package dev.shrews.services;
 import java.util.Set;
 
 import dev.shrews.beans.User;
+import dev.shrews.data.UserDAO;
+import dev.shrews.data.UserHibernate;
 import dev.shrews.exceptions.NonUniqueUsernameException;
 
 public class UserServiceImpl implements UserService {
+	
+	private UserDAO userDAO;
+	
+	public UserServiceImpl () {
+		userDAO = new UserHibernate();
+	}
 
 	@Override
 	public Integer addUser(User u) throws NonUniqueUsernameException {
-		// TODO Auto-generated method stub
-		return null;
+		return userDAO.add(u).getId();
 	}
 
 	@Override
@@ -27,13 +34,14 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public User getUserByName(String username) {
-		// TODO Auto-generated method stub
-		return null;
+		return userDAO.getByUsrname(username);
 	}
+	
 	@Override
 	public void deleteUser(User u) {
 		//userDao.delete(u);
 	}
+	
 	@Override
 	public void updateUser(User u) {
 		//userDao.update(u);
