5441f17e8d08c8f7d638d050450f3723941d7cf2
Kyle Williams <43432040+kyledwilly@users.noreply.github.com>
2020-12-23 14:25:53 -0700

    routes addition
    routes addition

--- BetterReads/betterReadsNG/src/app/app-routing.module.ts
+++ BetterReads/betterReadsNG/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShelfComponent } from './shelf/shelf.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'shelf', component:ShelfComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
