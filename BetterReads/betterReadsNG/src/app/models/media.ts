ae1b5f8927d1421a65d122bd9961e16de21307f0
mbaehr <markbaehr@u.boisestate.edu>
2020-12-26 14:25:26 -0600

    media controller working, addmedia component not sending http request

--- BetterReads/betterReadsNG/src/app/models/media.ts
+++ BetterReads/betterReadsNG/src/app/models/media.ts
@@ -7,4 +7,5 @@ export class Media {
     creator: String;
     mediaType: MediaType;
     genre: Genre;
+    publicationDate: Date;
 }
--- BetterReads/betterReadsNG/src/app/services/media.service.spec.ts
+++ BetterReads/betterReadsNG/src/app/services/media.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { Media.ServiceService } from './media.service.service';
+import { Media.ServiceService } from './media.service';
 
 describe('Media.ServiceService', () => {
   let service: Media.ServiceService;
