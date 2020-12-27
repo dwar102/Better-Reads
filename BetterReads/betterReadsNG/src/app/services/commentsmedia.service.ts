21ac4ec64fa71009d3e256b14ca9be5cc3a76b51
Brian Conlon <brian.conlon18@myhunter.cuny.edu>
2020-12-27 02:24:40 -0500

    Late night push- establishing frontend for media comments

--- BetterReads/betterReadsNG/src/app/services/commentsmedia.service.ts
+++ BetterReads/betterReadsNG/src/app/services/commentsmedia.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { UrlService } from '../url.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentsmediaService {
+  mediaCommentsUrl: string;
+  private formHeaders = new HttpHeaders({'Cookie':this.cookieService.get('JSESSIONID'),
+  'Content-Type': 'application/x-www-form-urlencoded'});
+  private regHeaders = new HttpHeaders({'Cookie':this.cookieService.get('JSESSIONID'),
+  'Content-Type':'application/json'});
 
-  constructor() { }
+  constructor(private http: HttpClient, private urlService: UrlService, private cookieService: CookieService) {
+    this.mediaCommentsUrl = this.urlService.getUrl() + 'media/comments/';
+   }
 }
