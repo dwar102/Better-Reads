14d162dcbea01932229308f0b79cd00869b7cabc
Brian Conlon <brian.conlon18@myhunter.cuny.edu>
2020-12-27 19:02:44 -0500

    Media comments moved to testing, now working on messaging.

--- BetterReads/betterReadsNG/src/app/app-routing.module.ts
+++ BetterReads/betterReadsNG/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RegisterComponent } from './register/register.component';
 import { ShelfComponent } from './shelf/shelf.component';
 import { ProfileComponent } from './profile/profile.component';
 import { AddmediaComponent } from './addmedia/addmedia.component';
+import { AddCommentComponent } from './add-comment/add-comment.component';
 
 const routes: Routes = [
   {path:'addmedia', component:AddmediaComponent},
   {path:'shelf', component:ShelfComponent},
   {path:'register', component:RegisterComponent},
-  {path:'profile', component:ProfileComponent}
+  {path:'profile', component:ProfileComponent},
+  {path:'writeComment', component:AddCommentComponent}
 ];
 
 @NgModule({
