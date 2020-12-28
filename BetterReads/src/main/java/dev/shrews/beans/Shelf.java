01489486efac303de0be2d8d5a5abb94d733d4fc
Kyle Williams <43432040+kyledwilly@users.noreply.github.com>
2020-12-28 10:27:33 -0700

    pushing some work
    pushing some work

--- BetterReads/src/main/java/dev/shrews/beans/Shelf.java
+++ BetterReads/src/main/java/dev/shrews/beans/Shelf.java
@@ -33,17 +33,41 @@ public class Shelf {
 	private User user;
 	@Column(name="shelf_name")
 	private String name;
-	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-	@JoinColumn(name = "shelf_assignment_id")//
-	@Autowired
-	private Set<ShelfAssignment> shelfAssignments;
+//	@OneToMany(fetch = FetchType.LAZY)
+//	@JoinColumn(name = "shelf_assignment_id")
+//	@Autowired
+//	private Set<ShelfAssignment> shelfAssignments;
 	
 	public Shelf(Integer id, User user, String name, Set<ShelfAssignment> shelfAssignments) {
 		super();
 		this.id = id;
 		this.user = user;
 		this.name = name;
-		this.shelfAssignments = shelfAssignments;
+//		this.shelfAssignments = shelfAssignments;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	@Autowired
@@ -53,71 +77,4 @@ public class Shelf {
 		this.user = new User();
 		this.name = "";
 	}
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public User getUser() {
-		return user;
-	}
-	public void setUser(User user) {
-		this.user = user;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public Set<ShelfAssignment> getShelfAssignments() {
-		return shelfAssignments;
-	}
-	public void setShelfAssignments(Set<ShelfAssignment> shelfAssignments) {
-		this.shelfAssignments = shelfAssignments;
-	}
-	@Override
-	public String toString() {
-		return "Shelf [id=" + id + ", name=" + name + ", shelfAssignments=" + shelfAssignments + "]";
-	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((shelfAssignments == null) ? 0 : shelfAssignments.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Shelf other = (Shelf) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (shelfAssignments == null) {
-			if (other.shelfAssignments != null)
-				return false;
-		} else if (!shelfAssignments.equals(other.shelfAssignments))
-			return false;
-		return true;
-	}
-
-	
-
-
 }
