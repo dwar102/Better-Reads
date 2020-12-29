30295cfaf7f9def66b0eb4b476c56bcdb20807cc
Brian Conlon <brian.conlon18@myhunter.cuny.edu>
2020-12-29 13:18:25 -0500

    Review service

--- BetterReads/betterReadsNG/src/app/models/review.ts
+++ BetterReads/betterReadsNG/src/app/models/review.ts
@@ -1,5 +1,11 @@
+import { Media } from "./media";
+import { User } from "./user";
+
 export class Review {
     id: Number;
+    date: Date;
+    rating: Number;
+    user: User;
+    media: Media;
     
-
 }
--- BetterReads/betterReadsNG/src/app/services/review-service.service.ts
+++ BetterReads/betterReadsNG/src/app/services/review-service.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { UrlService } from '../url.service';
+import { Observable, pipe } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Review } from '../models/review';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReviewServiceService {
-
-  constructor() { }
+reviewUrl: string;
+private formHeaders = new HttpHeaders({'Cookie':this.cookieService.get('JSESSIONID'),
+'Content-Type': 'application/x-www-form-urlencoded'});
+private regHeaders = new HttpHeaders({'Cookie':this.cookieService.get('JSESSIONID'),
+'Content-Type':'application/json'});
+  constructor(private http: HttpClient, private urlService: UrlService, private cookieService: CookieService) { 
+    this.reviewUrl = this.urlService.getUrl() + 'review';
+  }
+  getReviews(id: Number): Observable<Review[]> {
+    return this.http.get(this.reviewUrl + id, {withCredentials:true}).pipe(
+      map(resp => resp as Review[])
+    );
+  }
+  placeReview(review: Review): Observable<Review> {
+    return this.http.post(this.reviewUrl, review, {headers: this.regHeaders, withCredentials:true}).pipe(
+      map(resp => resp as Review)
+    );
+  }
 }
