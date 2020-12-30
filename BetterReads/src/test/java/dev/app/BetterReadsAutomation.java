31dd0d73307b9b2de9b43df2837855d02407c26a
Brian Conlon <brian.conlon18@myhunter.cuny.edu>
2020-12-30 15:27:10 -0500

    Test push & pom fix

---
+++ BetterReads/src/test/java/dev/app/BetterReadsAutomation.java
@@ -0,0 +1,30 @@
+package dev.app;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import pages.BetterReadsHome;
+
+public class BetterReadsAutomation {
+	public static void main(String[] args) {
+		File file = new File("src/test/resources/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+		
+		WebDriver driver = new ChromeDriver();
+		BetterReadsHome readHome = new BetterReadsHome(driver);
+		
+		readHome.navigateTo();
+		readHome.getUsername().sendKeys("hey");
+		readHome.getPassword().sendKeys("joe");
+		readHome.getLogin().click();
+		
+		driver.quit();
+	}
+}
