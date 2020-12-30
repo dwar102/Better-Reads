4c12ae97961ca3e347990156489a47772810bb25
Kyle Williams <43432040+kyledwilly@users.noreply.github.com>
2020-12-30 16:07:06 -0700

    adding and removing to shelf automatically update
    adding and removing to shelf automatically update

--- BetterReads/betterReadsNG/src/app/media/media.component.ts
+++ BetterReads/betterReadsNG/src/app/media/media.component.ts
@@ -6,6 +6,8 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { faPlusSquare } from '@fortawesome/free-solid-svg-icons';
 import { ShelfService } from '../shelf.service'
 import { Router } from '@angular/router';
+import { mediaComment } from '../models/mediacomment';
+import { Shelf } from '../models/shelf';
 @Component({
   selector: 'app-media',
   templateUrl: './media.component.html',
@@ -15,10 +17,44 @@ export class MediaComponent implements OnInit {
   faTrash = faTrash;
   faPlusSquare = faPlusSquare;
   @Input() public loggedUser: User; 
-  @Input() public items: Media[];
+  @Input() public items: ShelfAssignment[];
+  @Input() public shelfId: number;
+  
   constructor(private shelfService: ShelfService) { }
-
+  addMediaId: number;
+  sa : ShelfAssignment;
+  addMedia : Media;
+  shelf: Shelf;
+  
   ngOnInit(): void {
+  }
+
+  addShelfAssignment(){
+    // alert("want to add media with id: " + this.addMediaId + "to shelf with id: " + this.shelfId);
+    this.sa = new ShelfAssignment();
+    this.addMedia = new Media();
+    this.shelf = new Shelf();
+    this.addMedia.id = this.addMediaId;
+    this.sa.media = this.addMedia;
+    this.shelf.id = this.shelfId;
+    this.sa.shelf = this.shelf;
+    
+    this.shelfService.addShelfAssignment(this.sa).subscribe(
+      resp => {
+        // console.log("before delete");
+        // console.log(this.items);
+        // console.log("items length: " + this.items.length); 
+        // for(var i = 0; i < this.items.length; i++){
+        //   if(this.items[i].id == sa.id){
+            var newMedia = resp;
+            this.items.push(newMedia);
+        //   }
+        // }
+        // console.log("after delete");
+        // console.log(this.items);
+        // console.log("items length: " + this.items.length); 
+      }
+    );
   }
   removeShelfAssignment(sa : ShelfAssignment) {
     //alert("removeShelfAssignment: " + sa.id + " " + sa.nang me + " " + sa.shelf.id + " " + sa.user.username);
@@ -28,7 +64,7 @@ export class MediaComponent implements OnInit {
         console.log(this.items);
         console.log("items length: " + this.items.length); 
         for(var i = 0; i < this.items.length; i++){
-          if(this.items[0].id == sa.id){
+          if(this.items[i].id == sa.id){
             this.items.splice(i,1);
           }
         }
